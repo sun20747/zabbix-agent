4489c097f9ddd1a266c10264ca86fd2fb2033817
Atit khaoeam <78368053+sun20747@users.noreply.github.com>
2024-01-17 10:47:53 +0700

    Update install-zabbix-agent-ubuntu22.04.sh

--- install-zabbix-agent-ubuntu22.04.sh
+++ install-zabbix-agent-ubuntu22.04.sh
@@ -4,5 +4,9 @@ dpkg -i /tmp/zabbix-release_6.4-1+ubuntu22.04_all.deb
 apt update -y
 
 apt install zabbix-agent2 zabbix-agent2-plugin-*
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/zabbix/zabbix_agentd.conf
+sed -i 's/# ListenPort=10050/ListenPort=10050/g' /etc/zabbix/zabbix_agentd.conf
+sed -i 's/# Hostname=/Hostname='"${host}"'/g' /etc/zabbix/zabbix_agentd.conf
 systemctl restart zabbix-agent2
 systemctl enable zabbix-agent2
